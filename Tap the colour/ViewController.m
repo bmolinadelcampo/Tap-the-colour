7a9e0b05d751c00084472ee997b00e8387802b8d
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-03-17 17:30:10 +0000

    Create storyboard. Set challenge label and buttons text.

--- Tap the colour/ViewController.m
+++ Tap the colour/ViewController.m
@@ -7,8 +7,33 @@
 //
 
 #import "ViewController.h"
+#include <stdlib.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) NSArray *colours;
+@property (strong, nonatomic) NSArray *buttons;
+//@property (strong, nonatomic) UIColor *pink;
+//@property (strong, nonatomic) UIColor *yellow;
+//@property (strong, nonatomic) UIColor *purple;
+//@property (strong, nonatomic) UIColor *blue;
+//@property (strong, nonatomic) UIColor *green;
+@property (strong, nonatomic) NSString *solution;
+@property (strong, nonatomic) NSString *textForLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *whatColourToTouchLabel;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UIButton *topLeftButton;
+@property (weak, nonatomic) IBOutlet UIButton *topRightButton;
+@property (weak, nonatomic) IBOutlet UIButton *bottomLeftButton;
+@property (weak, nonatomic) IBOutlet UIButton *bottomRightButton;
+
+- (void)createNewChallenge;
+- (IBAction)topLeftButton:(id)sender;
+- (IBAction)topRightButton:(id)sender;
+- (IBAction)bottomLeftButton:(id)sender;
+- (IBAction)bottomRightButton:(id)sender;
+- (IBAction)nextButton:(id)sender;
 
 @end
 
@@ -16,12 +41,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.colours = @[@"pink", @"yellow", @"purple", @"blue", @"green"];
+    
+    self.buttons = @[self.topLeftButton, self.topRightButton, self.bottomLeftButton, self.bottomRightButton];
+    
+    [self createNewChallenge];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)createNewChallenge {
+    self.solution = self.colours[3];
+    self.whatColourToTouchLabel.text = [NSString stringWithFormat:@"Tap colour %@!", self.solution];
+    
+    for (UIButton *b in self.buttons) {
+        NSInteger randomIndex = arc4random() %4;
+        [b setTitle:self.colours[randomIndex] forState:UIControlStateNormal];
+        NSLog(@"Random index is %li", (NSInteger) randomIndex);
+    }
 }
+
+- (IBAction)topLeftButton:(id)sender {
+}
+
+- (IBAction)topRightButton:(id)sender {
+}
+
+- (IBAction)bottomLeftButton:(id)sender {
+}
+
+- (IBAction)bottomRightButton:(id)sender {
+}
+
+- (IBAction)nextButton:(id)sender {
+}
+
+
 
 @end
